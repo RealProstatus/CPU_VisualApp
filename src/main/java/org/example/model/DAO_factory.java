4a8be446dab84428e68d0da6a9d76462aade4391
prostatus <mel.sirvania@gmail.com>
2024-12-02 22:26:29 +0300

    it works!

--- src/main/java/org/example/model/DAO_factory.java
+++ src/main/java/org/example/model/DAO_factory.java
@@ -1,7 +1,7 @@
 package org.example.model;
 
 public class DAO_factory {
-    private static final DAO_memory dao = new DAO_memory();
+    private static final DAO_memory dao = new DAO_DB();
 
     static DAO_memory build(){
         return dao;
